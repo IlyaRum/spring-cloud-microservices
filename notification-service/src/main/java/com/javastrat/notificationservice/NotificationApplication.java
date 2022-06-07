da80a6f86df2e4393d36a99d79ba5cb90e3506ac
IlyaRum <tekstir@yandex.ru>
2022-06-07 11:39:25 +0300

    Initial commit

---
+++ notification-service/src/main/java/com/javastrat/notificationservice/NotificationApplication.java
@@ -0,0 +1,8 @@
+package com.javastrat.notificationservice;
+
+public class NotificationApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ registry/src/main/java/com/javastrat/registry/RegistryApplication.java
@@ -0,0 +1,8 @@
+package com.javastrat.registry;
+
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
